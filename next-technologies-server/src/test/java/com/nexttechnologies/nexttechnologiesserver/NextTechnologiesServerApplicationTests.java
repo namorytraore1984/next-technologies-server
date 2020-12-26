98add6b6d4da6bf37b1c260c898ae57235035776
Namory Traore <namorytraore1984@yahoo.fr>
2020-12-26 18:50:07 -0500

    Premier commit 1

---
+++ next-technologies-server/src/test/java/com/nexttechnologies/nexttechnologiesserver/NextTechnologiesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexttechnologies.nexttechnologiesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextTechnologiesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
